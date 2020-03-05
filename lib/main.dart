078aadc65c6a2a07a5e31dc7515a077169d16d68
ipan97 <ipantaupik97@gmail.com>
2020-03-05 15:47:30 +0700

    counter bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,79 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc_sample/color_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  ColorBloc bloc = ColorBloc();
-
-  @override
-  void dispose() {
-    super.dispose();
-    bloc.dispose();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        floatingActionButton: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            FloatingActionButton(
-              backgroundColor: Colors.amber,
+        title: "Flutter Counter Bloc",
+        home: BlocProvider<CounterBloc>(
+          create: (context) => CounterBloc(),
+          child: CounterPage(),
+        ));
+  }
+}
+
+class CounterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final CounterBloc counterBloc = BlocProvider.of<CounterBloc>(context);
+    return Scaffold(
+      appBar: AppBar(title: Text("Counter")),
+      body: BlocBuilder<CounterBloc, int>(
+        builder: (context, count) {
+          return Center(
+            child: Text(
+              '$count',
+              style: TextStyle(fontSize: 24.0),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: Icon(Icons.add),
               onPressed: () {
-                bloc.eventSink.add(ColorEvent.to_amber);
+                counterBloc.add(CounterEvent.increment);
               },
             ),
-            SizedBox(width: 10),
-            FloatingActionButton(
-              onPressed: () {
-                bloc.eventSink.add(ColorEvent.to_light_blue);
-              },
-              backgroundColor: Colors.lightBlue,
-            )
-          ],
-        ),
-        appBar: AppBar(
-          title: Text("Flutter Bloc Sample"),
-        ),
-        body: Center(
-          child: StreamBuilder(
-            stream: bloc.stateStream,
-            initialData: Colors.amber,
-            builder: (context, snapshot) {
-              return AnimatedContainer(
-                width: 100,
-                height: 100,
-                duration: Duration(microseconds: 500),
-                color: snapshot.data,
-              );
-            },
           ),
-        ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: Icon(Icons.remove),
+              onPressed: () {
+                counterBloc.add(CounterEvent.decrement);
+              },
+            ),
+          )
+        ],
       ),
     );
+  }
+}
+
+enum CounterEvent { increment, decrement }
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  @override
+  int get initialState => 0;
+
+  @override
+  Stream<int> mapEventToState(CounterEvent event) async* {
+    switch (event) {
+      case CounterEvent.decrement:
+        yield state - 1;
+        break;
+      case CounterEvent.increment:
+        yield state + 1;
+        break;
+    }
   }
 }
